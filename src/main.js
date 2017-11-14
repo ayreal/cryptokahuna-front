47dd07a9a9721509abe7487a9ab1a001363d1bc4
Ariel Herman <aeh.herman@gmail.com>
2017-11-14 09:02:25 -0500

    fetch working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+let data;
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("here");
+  refreshQuotes();
 });
+
+function fetchQuotes() {
+  const PATH = "https://min-api.cryptocompare.com";
+  const ROUTE = "/data/pricemulti?fsyms=BTC,ETH,DASH,ZEC,XMR,LTC&tsyms=USD";
+  fetch(`${PATH}${ROUTE}`)
+    .then(res => res.json())
+    .then(json => (data = json));
+}
+
+function refreshQuotes() {
+  // call fetchQuotes every 10 sec
+}
